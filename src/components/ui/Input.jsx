a66b5c4fc6d07a8afa3c2ca9cf1b1c8c1826646a
Rodrigo Ardila <rodrigo.ardila@correo.tdea.edu.co>
2026-09-22 00:13:18 -0500

    Se agrega prop "label"

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -3,7 +3,7 @@
 especificando las props que puede llevar cada input, también la estructura del label
 de cada campo y el estilo, esto con el fin de tener un código mas optimo y limpio
 en el formulario */
-export default function Input({type="text", placeholder, value, onChange, name}){
+export default function Input({label, type="text", placeholder, value, onChange, name}){
     return (
         <div style={{marginBottom: "10px"}}>
             {label && <label style={{ fontSize: '14px', fontWeight: '600', color: '#1e293b' }}>{label}</label>}
